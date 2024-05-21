dd626498dd2b7f11486e6efcbe9711a09067c2f9
Jaeyeon Jo <jojaeyeon.kr@gmail.com>
2024-05-21 09:02:59 +0900

    Change UI

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -4,10 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function parseQuery(){
+  const queryStr = document.location.search
+  const paramStrs = queryStr.substring(1).split('&');
+  console.debug(paramStrs)
+  const resultMap = {}
+  paramStrs.forEach((paramStr) => {
+    const [key, val] = paramStr.split('=')
+    resultMap[key] = val
+  })
+
+  return resultMap
+}
+
+function parsePath(){
+  return document.location.pathname.split('/',30).slice(1).map(str=> '/'+str)
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const paths = parsePath()
+const query = parseQuery()
 root.render(
   <React.StrictMode>
-    <App />
+    <App paths={paths} query={query}/>
   </React.StrictMode>
 );
 
